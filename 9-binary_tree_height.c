148526f090923cbd3409ed3013583505c2882caa
3k3n3 <80990247+3k3n3@users.noreply.github.com>
2023-08-04 11:44:30 +0100

    Correction: 9, 14

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,12 +8,15 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t l_height = 0;
+	size_t r_height = 0;
+
 	if (!tree)
 		return (0); /* If tree is NULL, return 0 */
 
-	size_t l_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-	size_t r_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	l_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	r_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 
 	/* Return max height of left & right subtree */
-	return ((l_height > r_height) ? l_height : r_height);
+	return (l_height > r_height ? l_height : r_height);
 }
